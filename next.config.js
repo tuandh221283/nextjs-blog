bed9cd4453d7f8ca61d69c4fed662da71fe4d178
Tuan Duong <tuan.duong@s3corp.com.vn>
2023-09-25 14:32:11 +0700

    First commit

---
+++ next.config.js
@@ -0,0 +1,34 @@
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    async rewrites() {
+        return [
+          {
+            source: '/api/:path*',
+            destination: 'http://localhost:3000/:path*',
+          },
+        ]
+      },
+	async headers() {
+    return [
+      {
+        source: '/path/(.*)',
+        "headers": [
+        { "key": "Access-Control-Allow-Credentials", "value": "true" },
+        { "key": "Access-Control-Allow-Origin", "value": "http://baradmin.barapp.localhost" }, // Change this to specific domain for better security
+        {
+          "key": "Access-Control-Allow-Methods",
+          "value": "GET,OPTIONS,PATCH,DELETE,POST,PUT"
+        },
+        {
+          "key": "Access-Control-Allow-Headers",
+          "value": "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
+        }
+      ],
+      },
+    ];
+  },
+}
+
+module.exports = nextConfig
