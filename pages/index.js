08bdc3d08f0714e55d29e47c6d6d8393920b9745
S3-Tuan Duong <tuan.duong@s3corp.com.vn>
2023-12-05 11:18:35 +0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -23,7 +23,7 @@ export default function Home({ allPostsData }) {
 
       {/* Add this <section> tag below the existing <section> tag */}
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Tuan Duong's Blog1</h2>
+        <h2 className={utilStyles.headingLg}>Tuan Duong's Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
